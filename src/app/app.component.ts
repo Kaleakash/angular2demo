de9e467d840e410e02b2b570880123df2e4d68c6
Akash <akash300383@gmail.com>
2017-12-06 18:20:46 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Welcome to Angular 2 with GitHub by Author Akash Kale';
 }
